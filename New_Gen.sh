0a2a8f020cee740697f8c9e98b574957ac76a7ac
zetamins <seerish8@gmail.com>
2025-12-29 01:17:43 +0400

    Update clone commands in New_Gen.sh

--- New_Gen.sh
+++ New_Gen.sh
@@ -22,11 +22,10 @@ echo ""
 # ==============================================================
 
 clones=(
-  "git clone -b bka https://github.com/zetamins/device_nothing_Spacewar.git device/nothing/Spacewar"
-  "git clone -b bka https://github.com/zetamins/kernel_nothing_sm7325.git kernel/nothing/sm7325"
-  "git clone -b bka https://github.com/zetamins/vendor_nothing_Spacewar_2.0.git vendor/nothing/Spacewar"
-  "git clone -b bka https://github.com/zetamins/hardware_nothing.git hardware/nothing"
-  "git clone -b derp16 https://github.com/zetamins/proprietary_vendor_nothing_camera.git vendor/nothing/camera"
+  "git clone -b axion https://github.com/smrth097/android_device_nothing_Spacewar.git device/nothing/Spacewar"
+  "git clone -b lineage-23.0 https://github.com/smrth097/kernel_nothing_sm7325.git kernel/nothing/sm7325"
+  "git clone -b bka https://github.com/smrth097/proprietary_vendor_nothing_Spacewar.git vendor/nothing/Spacewar"
+  "git clone -b bka https://github.com/smrth097/android_hardware_nothing.git hardware/nothing"
 )
 
 for cmd in "${clones[@]}"; do
